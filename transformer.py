43b509ef30d5701bba3a44b04a82236a5f3b164b
Radin Shayanfar <radin.shayanfar@gmail.com>
2025-06-07 14:29:48 +0330

    use pytorch linear layers for multi head attention

--- transformer.py
+++ transformer.py
@@ -6,23 +6,20 @@ class AttentionHead(nn.Module):
     def __init__(self, d_model: int, d_k: int):  # we assume d_k == d_v and only use d_k here
         super().__init__()
         self.scale = math.sqrt(d_k)
-        self.W_Q = nn.Parameter(torch.randn(d_model, d_k))
-        self.b_Q = nn.Parameter(torch.randn(1, d_k))
-        self.W_K = nn.Parameter(torch.randn(d_model, d_k))
-        self.b_K = nn.Parameter(torch.randn(1, d_k))
-        self.W_V = nn.Parameter(torch.randn(d_model, d_k))
-        self.b_V = nn.Parameter(torch.randn(1, d_k))
+        self.W_Q = nn.Linear(d_model, d_k)
+        self.W_K = nn.Linear(d_model, d_k)
+        self.W_V = nn.Linear(d_model, d_k)
 
         self.att_mask = torch.full((d_k, d_k), float("-inf")).triu(1)  # setting diagonal to 1 to exclude the main diagonal
 
     def forward(self, x, masked=False, enc_dec_key=None, enc_dec_value=None):
-        Q = x @ self.W_Q + self.b_Q
+        Q = self.W_Q(x)
         if enc_dec_key is None:  # this is not a encoder decoder attention layer, so we have to compute K and V
-            K = x @ self.W_K + self.b_K
+            K = self.W_K(x)
         else:
             K = enc_dec_key
         if enc_dec_value is None:  # this is not a encoder decoder attention layer, so we have to compute K and V
-            V = x @ self.W_V + self.b_V
+            V = self.W_V(x)
         else:
             V = enc_dec_value
         
@@ -38,17 +35,17 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, h, d_model, d_v):
         super().__init__()
         self.heads = [AttentionHead(d_model, d_v) for _ in range(h)]
-        self.W_O = nn.Parameter(torch.randn((h*d_v, d_model)))
-        self.b_O = nn.Parameter(torch.randn((1, d_model)))
+        self.W_O = nn.Linear(h*d_v, d_model)
 
     def forward(self, x, masked=False, enc_dec_key=None, enc_dec_value=None):
         zs = [head(x, masked, enc_dec_key, enc_dec_value) for head in self.heads]
         z = torch.cat(zs, dim=1)
-        z = z @ self.W_O + self.b_O
+        z = self.W_O(z)
 
         return z
 
-class EncoderBlock:
+
+class EncoderBlock(nn.Module):
     pass
 
 class DecoderBlock:
