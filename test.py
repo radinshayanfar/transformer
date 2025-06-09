fix attention mask nan propagation

test.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from transformer import AttentionHead, MultiHeadAttention, EncoderBlock, DecoderBlock, EncoderStack, DecoderStack, Transformer
 from utils import get_positional_encoding_table
 
@@ -6,71 +7,74 @@ if __name__ == "__main__":
     d_model = 16
     d_k = 4
     d_ff = 2048
-    head = AttentionHead(d_model, d_k)
-    att = head(torch.randn(1, 2, d_model))
-    print(att)
+    # head = AttentionHead(d_model, d_k)
+    # att = head(torch.randn(1, 2, d_model))
+    # print(att)
 
-    mult_head = MultiHeadAttention(8, d_model, d_k)
-    print(mult_head(torch.randn(1, 2, d_model)).shape)
+    # mult_head = MultiHeadAttention(8, d_model, d_k)
+    # print(mult_head(torch.randn(1, 2, d_model)).shape)
 
-    mult_head = MultiHeadAttention(1, d_model, d_k, masked=True)
-    print(mult_head(torch.randn(1, 2, d_model)).shape)
+    # mult_head = MultiHeadAttention(1, d_model, d_k, masked=True)
+    # print(mult_head(torch.randn(1, 2, d_model)).shape)
 
-    enc_block = EncoderBlock(8, d_model, d_k, d_ff)
-    block = enc_block(torch.randn(1, 2, d_model))
-    print(block.shape)
+    # enc_block = EncoderBlock(8, d_model, d_k, d_ff)
+    # block = enc_block(torch.randn(1, 2, d_model))
+    # print(block.shape)
 
-    dec_head = MultiHeadAttention(8, d_model, d_k)
-    att = dec_head(torch.randn(1, 2, d_model), torch.randn(1, 5, d_model))
-    print(att.shape)
+    # dec_head = MultiHeadAttention(8, d_model, d_k)
+    # att = dec_head(torch.randn(1, 2, d_model), torch.randn(1, 5, d_model))
+    # print(att.shape)
     
-    dec_block = DecoderBlock(8, d_model, d_k, d_ff)
-    block = dec_block(torch.randn(1, 2, d_model), torch.randn(1, 5, d_model))
-    print(block.shape)
+    # dec_block = DecoderBlock(8, d_model, d_k, d_ff)
+    # block = dec_block(torch.randn(1, 2, d_model), torch.randn(1, 5, d_model))
+    # print(block.shape)
 
-    dec_block = DecoderBlock(8, d_model, d_k, d_ff, decoder_only=True)
-    block = enc_block(torch.randn(1, 2, d_model))
-    print(block.shape)
+    # dec_block = DecoderBlock(8, d_model, d_k, d_ff, decoder_only=True)
+    # block = enc_block(torch.randn(1, 2, d_model))
+    # print(block.shape)
 
-    enc_stack = EncoderStack(6, 8, d_model, d_k, d_ff)
-    stack = enc_stack(torch.randn(1, 2, d_model))
-    print(stack.shape)
+    # enc_stack = EncoderStack(6, 8, d_model, d_k, d_ff)
+    # stack = enc_stack(torch.randn(1, 2, d_model))
+    # print(stack.shape)
 
-    dec_stack = DecoderStack(6, 8, d_model, d_k, d_ff)
-    stack = dec_stack(torch.randn(1, 2, d_model), torch.randn(1, 5, d_model))
-    print(stack.shape)
+    # dec_stack = DecoderStack(6, 8, d_model, d_k, d_ff)
+    # stack = dec_stack(torch.randn(1, 2, d_model), torch.randn(1, 5, d_model))
+    # print(stack.shape)
 
-    dec_stack = DecoderStack(6, 8, d_model, d_k, d_ff, True)
-    stack = dec_stack(torch.randn(1, 2, d_model))
-    print(stack)
+    # dec_stack = DecoderStack(6, 8, d_model, d_k, d_ff, True)
+    # stack = dec_stack(torch.randn(1, 2, d_model))
+    # print(stack)
 
-    print(get_positional_encoding_table(5, 16))
+    # print(get_positional_encoding_table(5, 16))
 
-    stack = dec_stack(torch.randn(10, 2, d_model))
+    # stack = dec_stack(torch.randn(10, 2, d_model))
 
-    transformer = Transformer(6, 8, d_model, d_k, d_ff, 100, 1024, arch="encoder")
-    output = transformer(torch.randint(0, 100, (4, 17)))
-    print(output.shape)
+    # transformer = Transformer(6, 8, d_model, d_k, d_ff, 100, 1024, arch="encoder")
+    # output = transformer(torch.randint(0, 100, (4, 17)))
+    # print(output.shape)
 
-    transformer = Transformer(6, 8, d_model, d_k, d_ff, 100, 1024, arch="decoder")
-    output = transformer(torch.randint(0, 100, (4, 17)))
-    print(output.shape)
+    # transformer = Transformer(6, 8, d_model, d_k, d_ff, 100, 1024, arch="decoder")
+    # output = transformer(torch.randint(0, 100, (4, 17)))
+    # print(output.shape)
 
-    transformer = Transformer(6, 8, d_model, d_k, d_ff, 100, 1024, arch="both")
-    output = transformer(torch.randint(0, 100, (4, 17)), torch.randint(0, 100, (4, 5)))
-    print(output.shape)
+    # transformer = Transformer(6, 8, d_model, d_k, d_ff, 100, 1024, arch="both")
+    # output = transformer(torch.randint(0, 100, (4, 17)), torch.randint(0, 100, (4, 5)))
+    # print(output.shape)
 
-    head = AttentionHead(d_model, d_k)
-    att = head(torch.randn(3, 3, d_model), padding_mask=torch.Tensor([[1, 1, 0], [1, 0, 0], [1, 1, 1]]))
-    print(att)
+    # print(output)
+    # exit()
 
-    transformer = Transformer(6, 8, d_model, d_k, d_ff, 100, 1024, arch="encoder")
-    output = transformer(torch.randint(0, 100, (4, 3)), padding_mask_x=torch.Tensor([[1, 1, 0], [1, 0, 0], [1, 1, 1], [1, 1, 1]]))
-    print(output.shape)
+    # head = AttentionHead(d_model, d_k)
+    # att = head(torch.randn(3, 3, d_model), padding_mask=torch.Tensor([[1, 1, 0], [1, 0, 0], [1, 1, 1]]))
+    # print(att)
 
-    transformer = Transformer(6, 8, d_model, d_k, d_ff, 100, 1024, arch="decoder")
-    output = transformer(torch.randint(0, 100, (4, 3)), padding_mask_x=torch.Tensor([[1, 1, 0], [1, 0, 0], [1, 1, 1], [1, 1, 1]]))
-    print(output.shape)
+    # transformer = Transformer(6, 8, d_model, d_k, d_ff, 100, 1024, arch="encoder")
+    # output = transformer(torch.randint(0, 100, (4, 3)), padding_mask_x=torch.Tensor([[1, 1, 0], [1, 0, 0], [1, 1, 1], [1, 1, 1]]))
+    # print(output.shape)
+
+    # transformer = Transformer(6, 8, d_model, d_k, d_ff, 100, 1024, arch="decoder")
+    # output = transformer(torch.randint(0, 100, (4, 3)), padding_mask_x=torch.Tensor([[1, 1, 0], [1, 0, 0], [1, 1, 1], [1, 1, 1]]))
+    # print(output.shape)
 
     transformer = Transformer(6, 8, d_model, d_k, d_ff, 100, 1024, arch="both")
     output = transformer(
@@ -80,3 +84,32 @@ if __name__ == "__main__":
         padding_mask_y=torch.Tensor([[1, 0], [1, 0], [1, 1], [1, 1]]),
     )
     print(output.shape)
+
+    loss_fn = nn.CrossEntropyLoss()
+
+    dummy_output = torch.zeros(([4, 2, 100]))
+    for i in range(4):
+        for j in range(2):
+            dummy_output[i, j, i*j+17] = 1
+
+    loss = loss_fn(output, dummy_output)
+
+    loss.backward()
+    print(output)
+    print(loss)
+
+    # transformer = Transformer(6, 1, d_model, d_k, d_ff, 100, 1024, arch="encoder")
+    # output = transformer(torch.randint(0, 100, (4, 3)), padding_mask_x=torch.Tensor([[1, 1, 0], [1, 0, 0], [1, 1, 1], [1, 1, 1]]))
+    # print(output)
+    # exit()
+
+    # transformer = Transformer(6, 1, d_model, d_k, d_ff, 100, 1024, arch="both")
+    # output = transformer(
+    #     torch.randint(0, 100, (4, 3)),
+    #     torch.randint(0, 100, (4, 2)),
+    #     padding_mask_x=torch.Tensor([[1, 1, 0], [1, 0, 0], [1, 1, 1], [1, 1, 1]]),
+    #     padding_mask_y=torch.Tensor([[1, 0], [1, 0], [1, 1], [1, 1]]),
+    # )
+    # print(output.shape)
+
+    # print(output)

transformer.py
@@ -39,9 +39,9 @@ class AttentionHead(nn.Module):
             padding_mask[padding_mask == 0] = float("-inf")
             padding_mask[padding_mask == 1] = 0
             scaled += padding_mask
-        # TODO: add batch attention mask for padded batches
         # softmaxing the scores accros each token, so that each row sums to 1
         attention = torch.softmax(scaled, dim=-1) @ V
+        attention = attention.nan_to_num()  # to prevent nan to propagate to other positions in multihead linear layer
         return attention
 
 